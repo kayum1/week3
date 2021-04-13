8dec897ff768a8c81b14d479be07095f5d76960c
Kayum Amzad <kayum2@gmail.com>
2021-04-13 16:31:37 -0400

    Add files via upload

---
+++ src/components/loadError/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+export const LoadError = styled.div`
+  width: 590px;
+  height: 590px;
+  overflow: hidden;
+  background: url('./loadingError.svg') 50% 50% no-repeat;
+  background-size: cover;
+`;
